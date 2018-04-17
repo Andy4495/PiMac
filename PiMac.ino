6ded13d35390cf8570c3be9e1e9e2e3e8acbdcb4
Andreas Taylor <Andy4495@outlook.com>
2018-04-17 15:26:54 -0500

    Initial commit.

---
+++ PiMac.ino
@@ -0,0 +1,265 @@
+// iMac G5 Raspberry Pi System Management Code: "PiMac"
+//  https://gitlab.com/Andy4495/PiMac
+//
+// Version 0.1 - 7/11/2017 - A.T. - Initial code
+// Version 0.2 - 7/12/2017 - A.T. - Re-organized into generic state-machine code with function pointers
+//                                - Added states to wait for button release
+//                                - Other clean-up
+// Version 1.0 - 4/16/2018 - A.T. - Release to GitLab
+//
+// Monitor power button to turn power supply on/off
+// Control front panel LED (future iterations will have improved LED pulsating effects)
+// Monitor POWER_GOOD signal from power supply
+// Write simple status messages and time stamps to hardware serial
+//
+
+const int POWER_BUTTON = 8;        // Active LOW input
+const int POWER_GOOD = 6;          // Active HIGH input
+const int PS_ON      = 4;          // Active HIGH output (activates low-side switch to pull ATX pin low)
+const int FRONT_LED  = 5;          // Active HIGH output
+const int LCD_BRIGHTNESS = 9;      // PWM output, 0-3.3V (not used in current iteration)
+
+typedef void *(*StateFunc)();
+
+// Declare the States here
+void *sleep_State();          // Powered down. Waiting for power button.
+void *powerUp_State();        // Powered up. Waiting for power button released.
+void *powerGood_State();      // Powered up. Waiting for POWER_GOOD signal.
+void *running_State();        // Powered up. Waiting for power button or loss of POWER_GOOD signal.
+void *shutdown_State();       // Powered down. Waiting for power button released.
+//
+
+StateFunc statefunc;  // Initial state
+StateFunc Laststatefunc;
+
+unsigned long currentTime;
+
+int buttonPressed = 0;               // Track whether button was pressed the last time through loop()
+unsigned long initialButtonTime;     // Track how long the power button has been pressed
+const int MIN_BUTTON_TIME = 100;     // Length in time in ms that power button needs to be pressed to turn on/off
+
+unsigned long pwrgoodTimer;         // Track time it takes to detect POWER_GOOD signal
+const int PWRGOOD_MAX_TIME = 750;   // ATX supplies should indicate POWER_GOOD within 500 ms
+
+unsigned long dimmerTime;
+unsigned long prevDimmerTime = 9999;
+int dimmerValue = 0;
+int upDown = 1; // Start off incrementing
+
+
+void setup() {
+
+  Serial.begin(9600);
+  currentTime = millis();
+  Serial.print("Welcome to PiMac: ");
+  Serial.println(currentTime);
+  digitalWrite(PS_ON, LOW); // Make sure pin is at inactive level when switched to output mode.
+  pinMode(PS_ON, OUTPUT);
+  digitalWrite(PS_ON, LOW); // Make sure pin is at inactive level when switched to output mode.
+  pinMode(FRONT_LED, OUTPUT);
+  //pinMode(LCD_BRIGHTNESS, OUTPUT); Not used at this time. DO NOT ACTIVATE unless 5->3.3 level shifting circuit in place!
+  pinMode(POWER_BUTTON, INPUT_PULLUP);
+  statefunc = &sleep_State;
+  // Debug code
+  Laststatefunc = NULL;
+  Serial.print("sleep_State: ");
+  Serial.println((long) &sleep_State);
+  Serial.print("powerUp_State: ");
+  Serial.println((long) &powerUp_State);
+  Serial.print("powerGood_State: ");
+  Serial.println((long) &powerGood_State);
+  Serial.print("running_State: ");
+  Serial.println((long) &running_State);
+  Serial.print("shutdown_State: ");
+  Serial.println((long) &shutdown_State);
+  //
+
+}
+
+void loop() {
+  currentTime = millis();
+  // Debug code
+  if (Laststatefunc != statefunc) {
+    Serial.print("currentTime: ");
+    Serial.println(currentTime);
+    Serial.print("statefunc: ");
+    Serial.println((long) statefunc);
+    Laststatefunc = statefunc;
+  }
+  //
+  statefunc = (StateFunc)(*statefunc)();
+}
+
+
+
+void * sleep_State() {
+
+  StateFunc retval = sleep_State;
+
+  BreatheLED();       // Pulse the LED while in sleep state
+
+  if (digitalRead(POWER_BUTTON) == LOW) { // See how long it has been LOW
+    if (buttonPressed == 0) {   // Not previously pressed, so start the counter
+      buttonPressed = 1;
+      initialButtonTime = currentTime;
+      Serial.print("Power ON button press detected: ");
+      Serial.println(currentTime);
+    }
+    else {
+      if (currentTime - initialButtonTime > MIN_BUTTON_TIME) {
+        buttonPressed = 0;
+        initialButtonTime = currentTime;  // Reset the button timer to make sure it is off for a period of time before active again
+        digitalWrite(PS_ON, HIGH);    // Turn on power supply
+        Serial.print("Power ON button exceeded threshold: ");
+        Serial.println(currentTime);
+        analogWrite(FRONT_LED, 128);
+        retval = powerUp_State;
+      }
+    }
+  }
+  else { // Power button HIGH, not pressed) {
+    buttonPressed = 0;
+  }
+  return retval;
+}
+
+void * powerUp_State() {
+
+  StateFunc retval = powerUp_State;
+
+  if (digitalRead(POWER_BUTTON) == HIGH) { // See how long it has been released (HIGH)
+    if (buttonPressed == 0) {   // Not previously released, so start the counter
+      buttonPressed = 1;
+      initialButtonTime = currentTime;
+      Serial.print("Power ON button release detected: ");
+      Serial.println(currentTime);
+    }
+    else {
+      if (currentTime - initialButtonTime > MIN_BUTTON_TIME) {
+        buttonPressed = 0;
+        analogWrite(FRONT_LED, 192);   // Turn LED on brighter
+        Serial.print("Power ON button release exceeded threshold: ");
+        Serial.println(currentTime);
+        pwrgoodTimer = currentTime;
+        retval = powerGood_State;
+      }
+    }
+  }
+  else { // Power button is LOW: not released yet
+    buttonPressed = 0;
+  }
+
+  return retval;
+}
+
+void * powerGood_State() {
+
+  if (digitalRead(POWER_GOOD) == HIGH) {
+    analogWrite(FRONT_LED, 255);   // Turn on the LED full brightness
+    buttonPressed = 0;
+    Serial.print("POWER_GOOD detected: ");
+    Serial.println(currentTime);
+    return running_State;
+  }
+  if (currentTime - pwrgoodTimer > PWRGOOD_MAX_TIME) {
+    buttonPressed = 0;
+    initialButtonTime = currentTime;
+    analogWrite(FRONT_LED, 0);
+    digitalWrite(PS_ON, LOW);
+    Serial.print("POWER_GOOD took too long: ");
+    Serial.println(currentTime);
+    return sleep_State;
+  }
+  return powerGood_State;
+}
+
+
+void * running_State() {
+
+  StateFunc retval = running_State;
+
+  if (digitalRead(POWER_BUTTON) == LOW) { // See how long it has been LOW
+    if (buttonPressed == 0) {   // Not previously pressed, so start the counter
+      buttonPressed = 1;
+      initialButtonTime = currentTime;
+      Serial.print("Power OFF button press detected: ");
+      Serial.println(currentTime);
+    }
+    else {
+      if (currentTime - initialButtonTime > MIN_BUTTON_TIME) {
+        buttonPressed = 0;
+        initialButtonTime = currentTime;  // Reset the button timer to make sure it is off for a period of time before active again
+        digitalWrite(PS_ON, LOW);    // Turn off power supply
+        Serial.print("Power OFF button exceeded threshold: ");
+        Serial.println(currentTime);
+        analogWrite(FRONT_LED, 128);  // Dim the LED until button is released
+        retval = shutdown_State;
+      }
+    }
+  }
+  else { // Power button HIGH, not pressed)
+    buttonPressed = 0;
+  }
+  return retval;
+}
+
+void * shutdown_State() {
+
+  StateFunc retval = shutdown_State;
+
+  if (digitalRead(POWER_BUTTON) == HIGH) { // See how long it has been released (HIGH)
+    if (buttonPressed == 0) {   // Not previously released, so start the counter
+      buttonPressed = 1;
+      initialButtonTime = currentTime;
+      Serial.print("Power OFF button release detected: ");
+      Serial.println(currentTime);
+    }
+    else {
+      if (currentTime - initialButtonTime > MIN_BUTTON_TIME) {
+        buttonPressed = 0;
+        analogWrite(FRONT_LED, 0);   // Turn LED fully off
+        Serial.print("Power OFF button release exceeded threshold: ");
+        Serial.println(currentTime);
+        retval = sleep_State;
+      }
+    }
+  }
+  else { // Power button still LOW: not released yet
+    buttonPressed = 0;
+  }
+  return retval;
+}
+
+void BreatheLED() {
+  if (currentTime != prevDimmerTime) {
+    dimmerTime = currentTime % 100;
+    if (dimmerTime == 0) {
+      prevDimmerTime = currentTime;
+      if (upDown == 1) {
+        if (dimmerValue > 145) {
+          dimmerValue += 15;
+        }
+        else {
+          dimmerValue += 5;
+        }
+        if (dimmerValue > 235) {
+          dimmerValue = 235;
+          upDown = 0;
+        }
+      }
+      else {
+        if (dimmerValue > 145) {
+          dimmerValue -= 15;
+        }
+        else {
+          dimmerValue -= 5;
+        }
+        if (dimmerValue < 0) {
+          dimmerValue = 0;
+          upDown = 1;
+        }
+      }
+      analogWrite(FRONT_LED, dimmerValue);
+    }
+  }
+}
---
+++ README.md
@@ -0,0 +1,148 @@
+PiMac
+=====
+
+The "PiMac" -- converting a 2005 [iMac G5][1] (Ambient Light Sensor) into a Raspberry Pi desktop.
+
+I had an old iMac G5 <reference> in storage which, while obsolete, still had a nice 1680x1050 LCD display that I didn't want to throw away.
+
+I had previously converted an old [iMac G4][2] to a 1680x1050 DVI display using a [procedure][4] by Dremel Junkie, and was now looking to re-purpose my old iMac G5.
+
+I came across this [project][3] by Kiwi, which converted an iMac G5 into a "Hackintosh" by reusing the display and power supply and replacing the system board with an X86 motherboard.
+
+I decided to use a similar procedure to convert my iMac into a Raspberry Pi: the "PiMac".
+
+![PiMac up and running][20]
+
+Procedure
+=========
+The major parts of the conversion follow Kiwi's detailed [procedure][5]. However, certain steps are changed to accommodate the Raspberry Pi vs. X86 motherboard that Kiwi uses. My project also has a simpler SMB implementation, and uses what I believe is an easier method of converting the LVDS wires to an HDMI connector.
+
+HDMI Conversion
+---------------
+Adafruit has an HDMI [breakout board][6] from Adafruit which makes soldering the tiny LVDS video wires much easier compared to the surface-mount connector in Kiwi's procedure. In addition, I used a separate breakout board for the power, ground, and VEDID circuitry.
+
+![HDMI connector breakout][21]
+![HDMI breakout board][22]
+
+Power Supply
+------------
+As mentioned in Kiwi's procedure, it is easiest to use an ATX extension cable so that you don't need to cut the original cable coming from the Apple power supply. I used this [cable][7] -- similar cables are available from other vendors.
+
+There are multiple power supply connections needed inside the case. I created a wiring harness from the ATX extension cable using various connectors from my spare parts bin.
+
+![Power Supply Harness][23]
+
+##### Power Supply Harness Connections #####
+Raspberry Pi Power (mini-USB) - ATX pins 4, 5:
+- 5V power
+
+Arduino Power and POWER_GOOD (3-pin JST) - ATX pins 7, 8, 9:
+- Red: 5 V (always-on/Vsb power)
+- Black: GND
+- Yellow: POWER_GOOD signal from power supply (active high)
+
+LCD Power (3-pin JST) - ATX pins 10, 11, 21:
+- Red: 5 V
+- Black: GND
+- Yellow: 12 V
+
+Inverter Power (4-pin standard Molex) - ATX pins 12, 16, 22:
+- Red: 3.3 V
+- Black: GND
+- Yellow: 24 V
+
+Power On (3-pin 0.1” mini-Molex) - ATX pins 14,15:
+- Red: POWER_ON signal (active low turns on power supply outputs)
+- Black: GND
+
+AUX connector (4-pin 0.1" female header) - ATX pins 1, 6, 13, 17 (unused in current project):
+- White: 3.3V
+- Black: GND
+- Red: 5 V
+- Yellow: 12 V
+
+AUX connector (2-pin 0.1” female mini Molex) - ATX pins 2, 3 (unused in current project):
+- Red: 3.3V
+- Black: GND
+
+AUX connector (4-pin standard Molex) - ATX pins 18, 19, 20 (unused in current project):
+- Red: 5 V
+- Black: GND
+- Yellow: 12 V
+
+System Board
+------------
+As in Kiwi's procedure, it is useful to keep a part of the iMac system board, particularly the section with the I/O ports, power connector, and front panel LED. Although my version of the system board was not quite the same as Kiwi's, I pretty much cut the board in the same way.
+
+However, the circuit connections for the power button and front panel LED were different.
+
+For the **power switch**, solder wires directly to pins 2 and 3 of the switch. When looking down at the top side of the PC board, the pins are numbered as follows:  
+
+        1    2
+    switch contacts
+          3
+
+For the **front panel LED**, solder wires directly to the SMD solder pads. Kiwi's procedure mentions using vias on the system board. However, my version of the system board did not have the same via connections. I therefore recommend just soldering directly to the LED pads.
+
+Note that when putting the case back together, it is important to get the light tube (conical shaped plastic tube) mounted correctly between the system board and the iMac case so that the LED light is properly projected to the front of the case.
+
+System Management Board
+-----------------------
+
+I created a simpler System Management Board (SMB). It uses an Arduino Pro Mini (5V) to monitor the iMac power button, control the front panel LED, and turn on the power supply. It uses the "always-on" Vsb signal from the power supply and in turn controls the PS_ON signal to the power supply and monitors the Power_Good signal once the supply is turned on.
+
+![SMB Schematic][24]
+![SMB Wiring][25]
+
+SMB Software
+------------
+The SMB software implements a simple state machine to monitor and control the power button and power supply state. It also creates the "breathing" effect on the front panel LED while the PiMac is sleeping.
+
+SMB Arduino [code][10]
+![SMB State Machine Diagram][26]
+
+Putting It All Together
+-----------------------
+I mounted the circuit boards using existing mounting posts in the case, routing the cables and providing strain relief as needed.
+
+I used short USB A-A extension cables to route the Raspberry Pi USB ports out the bottom of the iMac case. I also used a 40-pin flat ribbon cable to route the Raspberry Pi GPIO pins outside the case.
+
+![Final Assembly][27]
+
+Parts
+-----
+These are the parts used in my build. These are not set in stone -- alternatives can be used depending on what you may have available.
+* ATX power supply [extension cable][7]
+* Adafruit [HDMI breakout][6]
+* USB [extension cables][8]
+* HDMI [cable][9]
+* 40-pin flat ribbon cable: from parts bin
+* Internal power connectors: from parts bin
+* Breakout boards: from parts bin
+
+
+References
+----------
++ Kiwi's iMac to Hackintosh [article][3]
++ Kiwi's detailed build [thread][5]
++ Dremel Junkie [iMac G4][4]
+
+[1]: https://everymac.com/systems/apple/imac/specs/imac_g5_2.0_20.html
+[2]: https://everymac.com/systems/apple/imac/specs/imac_1.25_20_fp.html
+[3]: https://tonymacx86.com/threads/kiwis-20-imac-i5.139633/
+[4]: http://www.dremeljunkie.com/2011/11/all-in-one-20-imac-g4-genuine-tmds-to.html
+[5]: https://www.tonymacx86.com/threads/kiwis-next-project-imac-g5.107859/
+[6]: https://www.adafruit.com/product/3121
+[7]: https://www.amazon.com/gp/product/B00EZ50W36/
+[8]: https://www.amazon.com/gp/product/B00CJG2ZYM
+[9]: https://www.amazon.com/gp/product/B00DI88XEG/
+[10]: ./PiMac.ino
+
+[20]: jpg/PiMac.jpg
+[21]: jpg/HDMI_Breakout.jpg
+[22]: jpg/HDMI_Wiring.jpg
+[23]: jpg/Power_Supply_Wiring_Harness.jpg
+[24]: jpg/SMB_Schematic.jpg
+[25]: jpg/SMB_Wiring.jpg
+[26]: jpg/SMB_State_Machine.jpg
+[27]: jpg/Final_Assembly.jpg
